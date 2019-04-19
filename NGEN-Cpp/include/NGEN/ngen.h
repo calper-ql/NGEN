f9d40566819c0d15611bfe09bdf2bcaa988ad37d
Can Alper <calper.ql@gmail.com>
2019-04-19 15:30:56 -0400

    added common file + update for gitignore

---
+++ NGEN-Cpp/include/NGEN/ngen.h
@@ -0,0 +1,15 @@
+//
+// Created by calper on 4/19/19.
+//
+
+#ifndef NGEN_NGEN_H
+#define NGEN_NGEN_H
+
+#include <NGEN/IO.h>
+#include <NGEN/noise.h>
+#include <NGEN/Module.h>
+#include <NGEN/ModulePool.h>
+#include <NGEN/Property.h>
+#include <NGEN/SimpleModules.h>
+
+#endif //NGEN_NGEN_H
